6cdb88cdff9ecede52e5aac522c7a273f4606107
ManulaK <badm.kavinda@gmail.com>
2024-09-09 14:45:22 +0530

    finalized ppt2 demo

--- main.py
+++ main.py
@@ -21,9 +21,9 @@ app.include_router(user_router, prefix="/api/v1", tags=["user"])
 app.include_router(chat_router, prefix="/api/v1", tags=["chat"])
 
 # localhost
-if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="localhost", port=9000)
 
 # live
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=9000)
